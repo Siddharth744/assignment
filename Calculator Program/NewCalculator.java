8c71224a8a146359f91e3d6d9db152ba014d5fd8
Siddharth744 <patilsiddharth744@gmail.com>
2022-06-24 01:54:13 +0530

    division function added

--- Calculator Program/NewCalculator.java
+++ Calculator Program/NewCalculator.java
@@ -1,6 +1,27 @@
 package assignment;
 
 public class NewCalculator {
+
+public static void Division(int a, int b, int c){
+	  int div;
+	   div = (a+b+c)/100;
+	   
+	   System.out.println(div);
+	}
+	
+   public static void main(String[] args) {
+	Scanner s =new Scanner(System.in);
+	int a = s.nextInt();
+	int b = s.nextInt();
+	int c = s.nextInt();
+	
+	Division(a,b,c);
+	
+	
+}
+   
+}
+
 	
 
 }
